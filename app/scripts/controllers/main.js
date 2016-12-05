f894c2cb9d27fd0f850c8f0b08d0afd6aac9ec57
Markus Schwarzenauer <Maggo@Markuss-MBP.home>
2016-12-05 20:25:43 +0100

    changed some calculations and commented out the modals for testing purpose

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -59,47 +59,53 @@ angular.module('angularWebAppApp')
     };
 
     $scope.calculate = function() {
-      $scope.wholeCount = $scope.beerCount + $scope.vodkaCount + $scope.whiskeyCount;
+        $scope.wholeCount = $scope.beerCount + $scope.vodkaCount + $scope.whiskeyCount;
 
-      if($scope.wholeCount == 0){
+        if($scope.wholeCount == 0){
         return;
-      }
+        }
 
-      $scope.beerPercentage = ($scope.beerCount/$scope.wholeCount*100);
-      $scope.vodkaPercentage = ($scope.vodkaCount/$scope.wholeCount*100);
-      $scope.whiskeyPercentage = ($scope.whiskeyCount/$scope.wholeCount*100);
+        $scope.beerPercentage = ($scope.beerCount/$scope.wholeCount*100);
+        $scope.vodkaPercentage = ($scope.vodkaCount/$scope.wholeCount*100);
+        $scope.whiskeyPercentage = ($scope.whiskeyCount/$scope.wholeCount*100);
 
-      $scope.beerDifference = ($scope.beerPercentage - $scope.basePercentage).toFixed(2);
-      $scope.vodkaDifference = ($scope.vodkaPercentage - $scope.basePercentage).toFixed(2);
-      $scope.whiskeyDifference = ($scope.whiskeyPercentage - $scope.basePercentage).toFixed(2);
+        $scope.beerDifference = ($scope.beerPercentage - $scope.basePercentage).toFixed(2);
+        $scope.vodkaDifference = ($scope.vodkaPercentage - $scope.basePercentage).toFixed(2);
+        $scope.whiskeyDifference = ($scope.whiskeyPercentage - $scope.basePercentage).toFixed(2);
 
-      $scope.newBeerPrice = ($scope.beerPrice + ($scope.beerDifference)*$scope.differenceFaktorInCent).toFixed(2);
-      $scope.newVodkaPrice = ($scope.vodkaPrice + ($scope.vodkaDifference)*$scope.differenceFaktorInCent).toFixed(2);
-      $scope.newWhiskeyPrice = ($scope.whiskeyPrice + ($scope.whiskeyDifference)*$scope.differenceFaktorInCent).toFixed(2);
+        $scope.beerDifferenceInEuro = ($scope.beerDifference * $scope.differenceFaktorInCent).toFixed(2);
+        $scope.vodkaDifferenceInEuro = ($scope.vodkaDifference * $scope.differenceFaktorInCent).toFixed(2);
+        $scope.whiskeyDifferenceInEuro = ($scope.whiskeyDifference * $scope.differenceFaktorInCent).toFixed(2);
 
-      $scope.log();
+        $scope.newBeerPrice = (parseInt($scope.beerPrice) + parseInt($scope.beerDifferenceInEuro)).toFixed(2);
+        $scope.newVodkaPrice = (parseInt($scope.vodkaPrice) + parseInt($scope.vodkaDifferenceInEuro)).toFixed(2);
+        $scope.newWhiskeyPrice = (parseInt($scope.whiskeyPrice) + parseInt($scope.whiskeyDifferenceInEuro)).toFixed(2);
 
+        $scope.log();
     };
 
     $scope.log = function() {
 
-      console.log("WholeCount: "+$scope.wholeCount);
-      console.log("Basisprozentsatz: "+$scope.basePercentage);
+        console.log("WholeCount: "+$scope.wholeCount);
+        console.log("Basisprozentsatz: "+$scope.basePercentage);
 
-      console.log("Bieranteil: "+$scope.beerPercentage);
-      console.log("Bierabweichung: "+ $scope.beerDifference);
-      console.log("Alter Bierpreis: "+ $scope.beerPrice);
-      console.log("Neuer Bierpreis: "+ $scope.newBeerPrice);
+        console.log("Bieranteil: "+$scope.beerPercentage);
+        console.log("Bierabweichung: "+ $scope.beerDifference);
+        console.log("Alter Bierpreis: "+ $scope.beerPrice);
+        console.log("Bierabweichung in Cent: "+$scope.beerDifferenceInEuro);
+        console.log("Neuer Bierpreis: "+ $scope.newBeerPrice);
 
-      console.log("Vodkaanteil: "+$scope.vodkaPercentage);
-      console.log("Vodkaabweichung: "+ $scope.beerDifference);
-      console.log("Alter Vodkapreis: "+ $scope.vodkaPrice);
-      console.log("Neuer Vodkapreis: "+ $scope.newVodkaPrice);
+        console.log("Vodkaanteil: "+$scope.vodkaPercentage);
+        console.log("Vodkaabweichung: "+ $scope.beerDifference);
+        console.log("Alter Vodkapreis: "+ $scope.vodkaPrice);
+        console.log("Vodkaabweichung in Cent: "+$scope.vodkaDifferenceInEuro);
+        console.log("Neuer Vodkapreis: "+ $scope.newVodkaPrice);
 
-      console.log("Whiskeyanteil: "+$scope.whiskeyPercentage);
-      console.log("Whiskeyabweichung: "+ $scope.whiskeyDifference);
-      console.log("Alter Whiskeypreis: "+ $scope.whiskeyPrice);
-      console.log("Neuer Whiskeypreis: "+ $scope.newWhiskeyPrice);
+        console.log("Whiskeyanteil: "+$scope.whiskeyPercentage);
+        console.log("Whiskeyabweichung: "+ $scope.whiskeyDifference);
+        console.log("Alter Whiskeypreis: "+ $scope.whiskeyPrice);
+        console.log("Whiskeyabweichung in Cent: "+$scope.whiskeyDifferenceInEuro);
+        console.log("Neuer Whiskeypreis: "+ $scope.newWhiskeyPrice);
 
     };
 
